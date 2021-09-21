e403ca3ead680ff608bea7d072b76741ad540f46
FabianGraml <fabian.graml03@gmail.com>
2021-09-21 16:52:12 +0200

    Finished turmrechner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+interface TowerItem {
+  towerBase: number;
+  towerOperator: string;
+  towerFactor: number;
+  towerResult: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-template';
+  startValue: number = 0;
+  height: number = 0;
+  isValid: boolean = false;
+  towerBase: number = 0;
+  tower: Array<string> = [];
+  towerLine: string = '';
+  currentTowerNumber: number = 0;
+
+  calculate() {
+    this.tower = [];
+    this.towerBase = this.startValue;
+    for (let i = 2; i <= this.height; i++) {
+      this.towerLine =  (this.towerBase + '*' + i +'='+this.towerBase*i).toString();
+      this.tower.push(this.towerLine);
+      this.towerBase = this.towerBase*i;
+
+    }
+    for (let i = 2; i <= this.height; i++) {
+    
+      this.towerLine =  (this.towerBase + '/' + i +'='+this.towerBase/i).toString();
+      this.tower.push(this.towerLine);
+      this.towerBase =  this.towerBase/i;
+       
+    }
+    console.log(this.tower)
+  }
+
 }
