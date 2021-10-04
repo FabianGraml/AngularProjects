5ed60c8275d5fddbaaed0498f926fd2926812d53
unknown <fabian.graml03@gmail.com>
2021-10-04 15:33:01 +0200

    fixed errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+interface ICustomer{
+  id: number;
+  firstname: string,
+  lastname: string,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-template';
+
+  constructor(private http: HttpClient){}
+  
+  customers: ICustomer[] = [];
+  
+
+  getAllCustomers():void{
+    this.http.get<ICustomer[]>('http://localhost:8000/api/customers')
+      .subscribe(result => this.customers = result);
+
+
+  }
 }
