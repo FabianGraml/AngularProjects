45142a31a98ca14cd285ae9b3c934664b00c6e42
FabianGraml <fabian.graml03@gmail.com>
2021-10-04 19:14:44 +0200

    Added some packages + server running now

--- PlaylistEditor-Backend/src/app.ts
+++ PlaylistEditor-Backend/src/app.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import cors from "cors";
+
 
 require("dotenv").config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+  });
 //get all customers
 app.get("/api/customers", (req, res) => {
     console.log("Hello")
