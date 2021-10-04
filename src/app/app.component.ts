0bafd2f16d3d2ccf8ff96daca21e6aa3fc7768c6
unknown <fabian.graml03@gmail.com>
2021-10-04 15:57:44 +0200

    angular REST practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,26 @@ export class AppComponent {
   constructor(private http: HttpClient){}
   
   customers: ICustomer[] = [];
+  userToSearch: Number = 0;
+  userToDelete: Number = 0;
+  customer: ICustomer = {} as ICustomer;
   
 
   getAllCustomers():void{
     this.http.get<ICustomer[]>('http://localhost:8000/api/customers')
       .subscribe(result => this.customers = result);
-
-
+  }
+  getSingleUser():void{
+    this.http.get<ICustomer>('http://localhost:8000/api/customers/'+this.userToSearch)
+    .subscribe(result => {
+      console.dir(result)
+      this.customer = result;
+    });
+  }
+  deleteUser():void{
+    this.http.delete('http://localhost:8000/api/customers/'+this.userToDelete)
+    .subscribe(result => {
+      console.dir(result)
+    });
   }
 }
