ef0cf5f2d8a3624455b07d7bf5d3b39c36cb03bb
FabianGraml <fabian.graml03@gmail.com>
2021-10-21 18:06:04 +0200

    Frontend basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,14 @@ interface IPerson {
   lastname: string,
   gender: string
 }
-interface IMatch{
+interface IMatch {
   id: number,
   round: number,
   person1: IPerson,
   person2: IPerson,
   winner: IPerson,
 }
+
 
 @Component({
   selector: 'app-root',
@@ -26,34 +27,62 @@ export class AppComponent {
 
   persons: IPerson[] = [];
   matches: IMatch[] = [];
+
   roundNumber: number = 0;
+  statusClass: string = 'btn btn-outline-success';
 
   constructor(private http: HttpClient) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.http.get<IPerson[]>('http://localhost:5000/api/persons').subscribe((result) => this.persons = result)
   }
-  nextRound(){
+  genereateTournament() {
     this.matches = [];
-    this.http.get<IMatch[]>('http://localhost:5000/api/genereateRound').subscribe((result) => this.matches = result)
-    if(this.matches.length === 0){
-      this.http.get<IMatch[]>('http://localhost:5000/api/getUnplayedMatch').subscribe((result) => this.matches = result)
+    this.http.delete<IMatch[]>('http://localhost:5000/api/resetMatchPlan').subscribe((x => {
+      this.http.get<IMatch[]>('http://localhost:5000/api/genereateRound').subscribe((result) => { 
+        this.matches = result;
+        this.roundNumber = this.matches[0].round;
+      })
+    }))
+  }
+  nextRound() {
+    this.matches = [];
+    this.http.get<IMatch[]>('http://localhost:5000/api/genereateRound').subscribe((result) => {
+      this.matches = result;
+      this.roundNumber = this.matches[0].round;
+
+    })
+    if (this.matches.length === 0) {
+      this.http.get<IMatch[]>('http://localhost:5000/api/getUnplayedMatch').subscribe((result) => {
+        this.matches = result;
+        this.roundNumber = this.matches[0].round;
+      })
     }
-    this.roundNumber = this.matches.map(x => x.round)[0];  
     console.log(this.matches)
   }
-  setMatchWinner(mathchId: number, matchWinnerId: number){
+  setMatchWinner(mathchId: number, matchWinnerId: number, round: number) {
+    this.statusClass = 'btn btn-success';
     const body = {
       "matchId": mathchId,
       "matchWinnerId": matchWinnerId
     }
     this.http.post(`http://localhost:5000/api/setWinner`, body).subscribe(data => {
       console.log(data)
-     
-    
     })
+
+    if (round === 5) {
+      const person = this.persons.find(x => x.id == matchWinnerId);
+      alert("Winner is: " + person?.firstname + ' ' + person?.lastname)
+    }
+  }
+  isDisabled(): boolean{
+    if(this.roundNumber === 5){
+      return true;
+    }else{
+      return false
+    }
   }
 
 }
