53e14b2d0ff4f7ce1e0f91539d35fba10b92a91d
unknown <fabian.graml03@gmail.com>
2021-10-21 13:40:32 +0200

    Init tournamentPlanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+interface IPerson {
+  id: number,
+  firstname: string,
+  lastname: string,
+  gender: string
+}
+interface IMatch{
+  id: number,
+  round: number,
+  person1: IPerson,
+  person2: IPerson,
+  winner: IPerson,
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +23,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-template';
+
+  persons: IPerson[] = [];
+  matches: IMatch[] = [];
+  roundNumber: number = 0;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(){
+    this.http.get<IPerson[]>('http://localhost:5000/api/persons').subscribe((result) => this.persons = result)
+  }
+  nextRound(){
+    this.matches = [];
+    this.http.get<IMatch[]>('http://localhost:5000/api/genereateRound').subscribe((result) => this.matches = result)
+    if(this.matches.length === 0){
+      this.http.get<IMatch[]>('http://localhost:5000/api/getUnplayedMatch').subscribe((result) => this.matches = result)
+    }
+    this.roundNumber = this.matches.map(x => x.round)[0];  
+    console.log(this.matches)
+  }
+  setMatchWinner(mathchId: number, matchWinnerId: number){
+    const body = {
+      "matchId": mathchId,
+      "matchWinnerId": matchWinnerId
+    }
+    this.http.post(`http://localhost:5000/api/setWinner`, body).subscribe(data => {
+      console.log(data)
+     
+    
+    })
+  }
+
 }
