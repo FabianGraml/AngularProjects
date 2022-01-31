ed85ff43b9a192487f924e7dd4d9aea9b47649d0
Fabian Graml <86606410+FabianGraml@users.noreply.github.com>
2022-01-31 18:48:18 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-
 
 @NgModule({
   declarations: [
